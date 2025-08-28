037a6fda0c30da42c06d315ec68d7e79f5f8c20b
Soph <sofia.morgulchik@gmail.com>
2025-08-28 14:50:44 +0100

    removing saved courses functionality

--- frontend/src/components/SessionContainer/SessionContainer.jsx
+++ frontend/src/components/SessionContainer/SessionContainer.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+
 import { FaBookmark, FaBook } from 'react-icons/fa';
 import './SessionContainer.css';
 
 export default function SessionContainer({ topic, onLearnClick }) {
-  const [saved, setSaved] = useState(false);
+  // const [saved, setSaved] = useState(false);
 
   return (
     <div className="session-container">
@@ -18,11 +18,12 @@ export default function SessionContainer({ topic, onLearnClick }) {
         </button>
       </div>
 
-      <FaBookmark
+    {/* for now no saving functionality  for simplicity*/}
+      {/* <FaBookmark
         className={`save-icon ${saved ? 'saved' : ''}`}
         onClick={() => setSaved(!saved)}
         title={saved ? 'Saved' : 'Save for later'}
-      />
+      /> */}
     </div>
   );
 }
