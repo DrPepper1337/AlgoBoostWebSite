9ddfccd73982aae6b09bb432e8769d33ac7eacc5
a-nosnitram <stasymartinson@gmail.com>
2025-09-02 19:57:17 +0400

    feat: start implementing the logic for user management on the frontend

--- frontend/src/components/ManageUsers/ManageUsers.jsx
+++ frontend/src/components/ManageUsers/ManageUsers.jsx
@@ -1,2 +1,75 @@
 import "./ManageUsers.css";
 import HeaderNavBar from "../HeaderNavBar/Header";
+import { useState } from "react";
+
+export default function ManageUsers() {
+    const [admins, setAdmins] = useState([]);
+    const [members, setMembers] = useState([]);
+    const [whitelist, setWhitelist] = useState([]);
+
+    useEffec(() => {
+        (async () => {
+            try {
+                const token = localStorage.getItem('authToken');
+                if (!token) return;
+                const res = await axios.get('http://localhost:8080/api/get-admins', {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                const AdminsData = Array.isArray(res.data?.data) ? res.data.data : [];
+                setAdmins(AdminsData)
+            } catch (error) {
+                console.error('Failed to fetch admins:', err);
+            }
+        })
+    })
+
+    useEffec(() => {
+        (async () => {
+            try {
+                const token = localStorage.getItem('authToken');
+                if (!token) return;
+                const res = await axios.get('http://localhost:8080/api/get-members', {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                const MembersData = Array.isArray(res.data?.data) ? res.data.data : [];
+                setMembers(MembersData)
+            } catch (error) {
+                console.error('Failed to fetch members:', err);
+            }
+        })
+    })
+
+    useEffec(() => {
+        (async () => {
+            try {
+                const token = localStorage.getItem('authToken');
+                if (!token) return;
+                const res = await axios.get('http://localhost:8080/api/get-whitelist', {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                const MembersData = Array.isArray(res.data?.data) ? res.data.data : [];
+                setWhitelist(MembersData)
+            } catch (error) {
+                console.error('Failed to fetch whitelist:', err);
+            }
+        })
+    })
+
+    return (
+        <div className="manage-users-wrapper">
+            <HeaderNavBar />
+            <h1>Manage Admins</h1>
+            <div className="admin-list">
+                {/* {admins.map((a) => (
+
+                ))} */}
+            </div>
+            <h1>Manage Members</h1>
+            <div className="member-list">
+            </div>
+            <h1>Manage Whitelist</h1>
+            <div className="white-list">
+            </div>
+        </div>
+    )
+}
