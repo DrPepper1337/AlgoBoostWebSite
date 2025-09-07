7097b7d34a53adf41ac9a18ee5b4dad2e94d6669
a-nosnitram <stasymartinson@gmail.com>
2025-09-08 00:54:57 +0200

    feat: mobile adjustments for navbar

--- frontend/src/components/HeaderNavBar/Header.jsx
+++ frontend/src/components/HeaderNavBar/Header.jsx
@@ -5,6 +5,8 @@ import { FaUser } from 'react-icons/fa';
 import './Header.css';
 import '../../styles/Global.css';
 import DropDownProfile from '../DropDownProfile/DropDownProfile';
+import { useMediaQuery } from 'react-responsive';
+
 
 export default function Header() {
   const navigate = useNavigate();
@@ -14,6 +16,7 @@ export default function Header() {
   const location = useLocation();
   const menuRef = useRef(null);
   const isHomePage = location.pathname === '/';
+  const isMobile = useMediaQuery({ maxWidth: 768 });
 
   useEffect(() => {
     function handleClickOutside(event) {
@@ -48,9 +51,16 @@ export default function Header() {
     <header className="site-header">
       <div className="header-inner">
 
-        <div className="logo-container">
-          <img src="/logo-no-text.svg" alt="AlgoBoost Logo" className="logo" />
-        </div>
+        {(isAuthenticated && isMobile) ?
+          <div className="user-menu" ref={menuRef}>
+            <FaUser className="user-menu-button" onClick={() => setOpen((prev) => !prev)} />
+            {open && <DropDownProfile isActive={open} />}
+          </div>
+          :
+          <div className="logo-container">
+            <img src="/logo-no-text.svg" alt="AlgoBoost Logo" className="logo" />
+          </div>
+        }
 
         <button
           className="menu-toggle"
@@ -58,16 +68,37 @@ export default function Header() {
           aria-label="Toggle menu"
         >
           <div className={`hamburger ${isMenuOpen ? 'open' : ''}`}>
-            {/* {
-              isMenuOpen ?
-                <div className="menu-btns-hamburger">
-                  <button className="menu-btn" id="login" onClick={() => { navigate('/login'); handleLinkClick(); }}>Login</button>
-                  <button className="menu-btn" id="register" onClick={() => { navigate('/login?mode=register'); handleLinkClick(); }}>Register</button>
-                  <button className="menu-btn" id="become-a-member" onClick={() => { window.open('https://www.yourunion.net/activities/societies/explore/algoboostsociety'); handleLinkClick(); }}>Become a Member</button>
-                </div>
-                : <></>
-            } */}
-                    </div>
+            <div className={`nav-container ${isMenuOpen ? 'open' : ''}`}>
+              {isAuthenticated && (<nav className="navbar">
+                {!isAuthenticated && !(location.pathname === '/login') && (
+                  <a className="navbarLink" id="login" onClick={() => { navigate('/login'); handleLinkClick(); }}>Login</a>
+                )}
+                {!isAuthenticated && !(location.pathname === '/register') && (
+                  <a className="navbarLink" id="register" onClick={() => { navigate('/login?mode=register'); handleLinkClick(); }}>Register</a>
+                )}
+                {!isAuthenticated && !(location.pathname === '/register') && (
+                  <a className="navbarLink" id="become-a-member" onClick={() => { window.open('https://www.yourunion.net/activities/societies/explore/algoboostsociety'); handleLinkClick(); }}>Become a Member</a>
+                )}
+
+                {isHomePage && !isAdmin && (<a className="navbarLink" href="/#about" onClick={handleLinkClick}>About us</a>)}
+                {isHomePage && !isAdmin && (<a className="navbarLink" href="/#offer" onClick={handleLinkClick}>What we offer</a>)}
+                {isHomePage && !isAdmin && (<a className="navbarLink" href="/#events" onClick={handleLinkClick}>Events</a>)}
+                {isHomePage && !isAdmin && (<a className="navbarLink" href="/#documents" onClick={handleLinkClick}>Documents</a>)}
+                {!isHomePage && !isAuthenticated && (<button className="navbarLink" onClick={() => { navigate('/'); handleLinkClick(); }}>Welcome Page</button>)}
+
+                {!isHomePage && (<button className="navbarLink" onClick={() => { navigate('/'); handleLinkClick(); }}>Welcome Page</button>)}
+                {!(location.pathname === '/memberHub') && (<button className="navbarLink" onClick={() => { navigate('/memberHub'); handleLinkClick(); }}>Member Hub</button>)}
+                {!(location.pathname === '/lessons') && (<button className="navbarLink" onClick={() => { navigate('/lessons'); handleLinkClick(); }}>Lessons</button>)}
+                {isAdmin && (
+                  <>
+                    {!(location.pathname === '/manageUsers') && (<button className="navbarLink admin-link" onClick={() => { navigate('/manageUsers'); handleLinkClick(); }}>Manage Users</button>)}
+                    {!(location.pathname === '/manageLessons') && (<button className="navbarLink admin-link" onClick={() => { navigate('/manageLessons'); handleLinkClick(); }}>Manage Lessons</button>)}
+                  </>
+                )}
+              </nav>
+              )}
+            </div>
+          </div>
         </button>
 
         <div className="right-controls">
@@ -86,7 +117,7 @@ export default function Header() {
         </div>
       </div>
 
-      <div className={`nav-container ${isMenuOpen ? 'open' : ''}`}>
+      <div className={`nav-container`}>
         {isHomePage && !isAdmin && (
           <nav className="navbar">
             <a className="navbarLink" href="/#about" onClick={handleLinkClick}>About us</a>
